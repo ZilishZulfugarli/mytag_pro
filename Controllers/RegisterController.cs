4e5619e36af92232ade43c8da534ed097384c6cb
ZilishZulfugarli <2002zilis@gmail.com>
2023-11-18 15:57:58 +0400

    MVC elave edildi

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using yeni_layihe.Models;
+
+namespace yeni_layihe.Controllers;
+
+public class RegisterController : Controller
+{
+    private readonly ILogger<RegisterController> _logger;
+
+    public RegisterController(ILogger<RegisterController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
